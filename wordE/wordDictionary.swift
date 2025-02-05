e3bd8462e2956a6a79d1b3b4ae0c81177ca1ecbe
EunJinCHO <till5941@gmail.com>
2025-02-05 11:30:37 +0900

    feat: wordDictionary isBookmarked 추가

---
+++ wordE/wordDictionary.swift
@@ -0,0 +1,26 @@
+//
+//  wordDictionary.swift
+//  wordE
+//
+//  Created by 존진 on 2/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class wordDictionary {
+    var word: String
+    var meaning: String
+    var imgPath: String
+    var isBookmarked: Bool
+    var isCorrect: Bool
+    
+    init(word: String, meaning: String, imgPath: String, isBookmarked:Bool = false, isCorrect: Bool = false) {
+        self.word = word
+        self.meaning = meaning
+        self.imgPath = imgPath
+        self.isBookmarked = isBookmarked
+        self.isCorrect = isCorrect
+    }
+}
